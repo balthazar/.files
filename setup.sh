97f162164bdd2ea75ff5a4d89d8b4f5d3d515763
Balthazar Gronon <bgronon@gmail.com>
2017-03-21 12:56:46 -0700

    refactor(setup): update, nvim + vim-plug

--- setup.sh
+++ setup.sh
@@ -2,21 +2,25 @@
 
 printf "\n[Basics]...\n"
 mkdir ~/git
-mkdir ~/.vim
-cd ~/git
-git clone git@github.com:Apercu/.files.git
+cd ~/git && git clone git@github.com:Apercu/.files.git
 cd ~
 
-printf "\n[Vim]...\n"
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+printf "\n[NVim]...\n"
 
-printf "\n[Creating Symlinks & Copy]...\n"
+mkdir -p ~/.config/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+printf "\n[OMZSH]...\n"
 
 curl -L http://install.ohmyz.sh | sh
 rm .zshrc
+
+printf "\n[Creating Symlinks & Copy]...\n"
+
 /bin/ln -s git/.files/.profile
-/bin/ln -s git/.files/.vimrc
 /bin/ln -s git/.files/.zshrc
 /bin/ln -s git/.files/.tigrc
 /bin/ln -s git/.files/.gitprompt
 /bin/ln -s git/.files/.gitconfig
+/bin/ln -s git/.files/init.vim ~/.config/nvim/init.vim
